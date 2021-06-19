4dc3c9cd2ad1b29803c5d2f3105484dc7d92a4bf
nguyencaothai <nguyencaothaiantn@gmail.com>
2021-06-19 16:07:23 +0000

    Change some options in command for running nikto

--- nikto_tool/nikto_tool.py
+++ nikto_tool/nikto_tool.py
@@ -1,6 +1,7 @@
 import subprocess
 import json
 import os
+from tldextract import extract
 
 def getDataFromNikto(url, token):
 
@@ -8,8 +9,19 @@ def getDataFromNikto(url, token):
 
     reportName = 'nikto_' + token + '.report'    
     subprocess.run(['rm', reportName], cwd='/root/python_tool/nikto_tool')
+
+    tsd, td, tsu = extract(url)
+    url = tsd + '.' + td + '.' + tsu
     
-    results = subprocess.run(['nikto','-h',url,'-Format','json', '-o', reportName, '-maxtime', '600s'], capture_output=True, cwd='/root/python_tool/nikto_tool/')
+    url = url[1: len(url)-1] if (url[0] == '.' and url[len(url)-1] == '.') else url[1:] if (url[0] == '.') else url[:len(url)-1] if (url[len(url)-1] == '.') else url
+    
+    port = '80'
+    if "http://" in url:
+        port = '80'
+    if "https://" in url:
+        port = '443'
+    
+    results = subprocess.run(['nikto','-h', url, '-p', port, '-Format','json', '-o', reportName, '-maxtime', '600s'], capture_output=True, cwd='/root/python_tool/nikto_tool/')
     if (results.returncode != 1):
         return True
     else:
